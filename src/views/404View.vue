444faa5c6838a1349a916d8d9504c12f2604196c
ArnautVasseur <vasseur.arnaut@gmail.com>
2023-11-24 10:33:53 +0100

    create 404 view

---
+++ src/views/404View.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    <img src="images/IV_logo_.JPG" alt="logo_pedicure_podologue" class="w-[71.97px] h-[75px] top-5 left-5">
+  </header>
+</template>
+
+<style>
+
+</style>
