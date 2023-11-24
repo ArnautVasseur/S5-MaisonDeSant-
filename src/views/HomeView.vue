8b1c76463e4b08d244cda2be708c80fb35a92c38
aeccher <95070759+aeccher@users.noreply.github.com>
2023-11-24 10:20:37 +0100

    Création des composants des boutons, de la carte avec le bouton et du formulaire de devis

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,15 @@
 <script setup>
-
+import CardButton from "../composants/CardButton.vue";
 </script>
 
 <template>
-    <h1>Homeview</h1>
+  <h1>Homeview</h1>
+  <CardButton
+    nom="Name"
+    name="Desktop Button with icon"
+    diplomes="Diplômes"
+    imageSrc="https://placekitten.com/200/200"
+  />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
