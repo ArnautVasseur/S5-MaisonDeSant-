d1696a9ef9baf98734a0fe59d026839caa6cb1e6
aeccher <95070759+aeccher@users.noreply.github.com>
2023-11-24 11:21:44 +0100

    Création du composant du header

---
+++ src/composants/Comp_Header.vue
@@ -0,0 +1,49 @@
+<script setup>
+import Logo from "../composants/Comp_Logo.vue";
+</script>
+
+<template>
+  <div class="flex bg-secondary-beige h-16 items-center">
+    <Logo class="ml-10" />
+    <nav>
+      <ul class="flex font-quicksand gap-14 ml-80">
+        <li class="flex items-center gap-2">
+          <a href=""></a>
+          <img
+            class="w-5"
+            src="/public/icons/footprint-icon.svg"
+            alt="Icone d'un pied"
+          />
+          Semelles orthopédiques
+        </li>
+        <li class="flex items-center gap-2">
+          <a href=""></a>
+          <img
+            class="w-5"
+            src="/public/icons/Medical-icon.svg"
+            alt="Icone d'un stéthoscope"
+          />
+          Traitements
+        </li>
+        <li class="flex items-center gap-2">
+          <a href=""></a>
+          <img
+            class="w-5"
+            src="/public/icons/map-icon.svg"
+            alt="Icone d'un pointeur"
+          />
+          Nous connaitre
+        </li>
+        <li class="font-semibold flex items-center gap-2">
+          <a href=""></a>
+          <img
+            class="w-5"
+            src="/public/icons/icon-devis.svg"
+            alt="Icone d'un document"
+          />
+          Faire mon devis
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
