6562d83c5f59dff9d7968784ea6c65e7d86f89a4
aeccher <95070759+aeccher@users.noreply.github.com>
2023-11-24 08:36:05 +0100

    Création du composant card

---
+++ src/components/Card.vue
@@ -0,0 +1,28 @@
+<script setup>
+const props = defineProps({
+  imageSrc: String,
+  imageAlt: String,
+  name: String,
+  description: String,
+});
+</script>
+
+<template>
+  <div
+    class="p-5 bg-white drop-shadow-[5px_5px_0px_rgba(221,244,234,1)] rounded-xl overflow-hidden justify-start items-center gap-5 inline-flex"
+  >
+    <img :src="imageSrc" :alt="imageAlt" class="rounded-lg" />
+    <div class="flex-col justify-start items-start inline-flex">
+      <div
+        class="w-[16.313rem] h-[1.625rem] text-black text-base font-semibold font-raleway"
+      >
+        {{ name }}
+      </div>
+      <div
+        class="w-[16.313rem] h-[6.188rem] text-black font-normal leading-6 font-quicksand"
+      >
+        {{ description }}
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,14 @@
 <script setup>
-
+import Card from "../components/Card.vue";
 </script>
 
 <template>
-
+  <Card
+    name="Name"
+    description="Description"
+    imageSrc="https://placekitten.com/200/200"
+    imageAlt=""
+  />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
