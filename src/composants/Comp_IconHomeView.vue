7dedbaa71579332b2197747eb4c588c6e810109b
aeccher <95070759+aeccher@users.noreply.github.com>
2023-12-15 14:14:28 +0100

    Modification du début de la page d'accueil

---
+++ src/composants/Comp_IconHomeView.vue
@@ -0,0 +1,25 @@
+<script setup>
+const props = defineProps({
+  imageSrc: String,
+  imageAlt: String,
+  title: String,
+  text: String,
+});
+</script>
+
+<template>
+  <div class="w-60">
+    <div class="w-24 h-24 bg-[#EAF4EB] rounded-full m-auto"></div>
+    <img
+      :src="imageSrc"
+      :alt="imageAlt"
+      class="sm:w-14 w-5 absolute -mt-[74px] ml-[92px]"
+    />
+    <p
+      class="text-primary_blue text-desktop_h3 font-quicksand font-semibold text-center mt-7"
+    >
+      {{ title }}
+    </p>
+    <p class="font-quicksand mt-2 text-center">{{ text }}</p>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <script setup>
 import Comp_Header from "../composants/Comp_Header.vue";
-import Comp_CardPathologies from "../composants/Comp_CardPathologies.vue";
+import Comp_IconHomeView from "../composants/Comp_IconHomeView.vue";
 import Button from "../composants/Buttons/Comp_Button.vue";
 import Comp_Card from "../composants/Comp_Card.vue";
 import Comp_CardButton from "../composants/Comp_CardButton.vue";
@@ -10,82 +10,86 @@ import Comp_Footer from "../composants/Comp_Footer.vue";
 <template>
   <Comp_Header />
 
-  <div class="h-[600px] bg-gradient-to-r from-[#124559] to-white"></div>
+  <div class="h-[600px] bg-gradient-to-r from-[#124559] to-white">
+    <h3
+      class="uppercase text-[#AEC3B0] font-quicksand font-semibold text-desktop_h3 pt-32 pl-24"
+    >
+      Vos pieds entre de bonnes mains
+    </h3>
+    <h2 class="text-white font-raleway font-semibold text-[5rem] pt-5 pl-24">
+      Votre Confort,
+    </h2>
+    <h2 class="text-white font-raleway font-semibold text-[5rem] pl-24">
+      Notre expertise
+    </h2>
+    <p class="font-quicksand text-white text-desktop_h4 opacity-75 pt-5 pl-24">
+      Des semelles orthopédiques modulé avec précision en réponse à votre
+      diagnostic.
+    </p>
+
+    <Router-Link to="/traitement">
+      <button
+        class="ml-24 mt-9 rounded-3xl bg-dark_primary_green font-quicksand font-semibold text-white text-desktop px-10 h-10 hover:bg-hover-green duration-150 ease-out"
+      >
+        Découvrir
+      </button></Router-Link
+    >
+
+    <!--<img src="../../public/images/semelles.jpg" alt="Une paire de semelles" />-->
+  </div>
 
   <h2
-    class="sm:text-desktop_h2 text-desktop_h4 font-raleway font-semibold text-dark_primary_green ml-10 mt-10"
+    class="sm:text-desktop_h1 text-center text-desktop_h4 font-raleway font-semibold text-dark_primary_green ml-10 mt-10"
   >
-    <span class="text-primary_blue">Des semelles orthopédiques</span>
+    <span class="text-black">Des semelles orthopédiques<br /></span>
     pour tous
   </h2>
 
-  <div class="grid sm:grid-cols-3 grid-cols-1 mt-10">
-    <div class="flex">
-      <img
-        class="sm:w-10 w-5 ml-10 mr-10"
-        src="/public/icons/face-icon.svg"
-        alt="Icone d'un visage"
-      />
-      <Comp_CardPathologies
-        title="ENFANTS"
-        text="Des semelles stimulantes et correctrices évoluant tout au long de la croissance."
-      />
-    </div>
-    <div class="flex">
-      <img
-        class="sm:w-10 w-5 ml-10 mr-10"
-        src="/public/icons/old-icon.svg"
-        alt="Icone d'une personne agée"
-      />
-      <Comp_CardPathologies
-        title="SÉNIORS"
-        text="Des semelles confortables et respectueuses des déformations pour toutes les pathologies."
-      />
-    </div>
-    <div class="flex">
-      <img
-        class="sm:w-10 w-5 ml-10 mr-10"
-        src="/public/icons/sport02-icon.svg"
-        alt="Icone d'un sportif"
-      />
-      <Comp_CardPathologies
-        title="SPORTIFS"
-        text="Des semelles plus légères et dynamiques pour de meilleures performances."
-      />
-    </div>
-    <div class="flex mt-10">
-      <img
-        class="sm:w-10 w-5 ml-10 mr-10"
-        src="/public/icons/shoe-icon.svg"
-        alt="Icone d'une chaussures "
-      />
-      <Comp_CardPathologies
-        title="CHAUSSURES ÉTROITES"
-        text="Des semelles correctrices plus fines, adaptées à un faible volume chaussant."
-      />
-    </div>
-    <div class="flex mt-10">
-      <img
-        class="sm:w-10 w-5 ml-10 mr-10"
-        src="/public/icons/clock-icon.svg"
-        alt="Icone d'une horloge"
-      />
-      <Comp_CardPathologies
-        title="AFFECTION DE LONGUE DURÉE"
-        text="Des semelles conformes à la législation et au protocole ALD (Affection de Longue Durée)."
-      />
-    </div>
-    <div class="flex mt-10">
-      <img
-        class="sm:w-10 w-5 ml-10 mr-10"
-        src="/public/icons/foot02-icon.svg"
-        alt="Icone d'un os du pied"
-      />
-      <Comp_CardPathologies
-        title="PIED DIABÉTIQUE"
-        text="Une optimisation des zones de décharges pour une meilleure répartition des appuis."
-      />
-    </div>
+  <p class="text-center text-[#5A5A5A] text-desktop_h4 font-quicksand mt-4">
+    Nos semelles sont pensées pour convenir au plus grand nombre de <br />
+    personne et pour toutes vos activitées.
+  </p>
+
+  <div class="grid sm:grid-cols-3 grid-cols-1 sm:mt-10 sm:justify-items-center">
+    <Comp_IconHomeView
+      imageSrc="/public/icons/face-icon.svg"
+      imageAlt="Icone d'un visage"
+      title="ENFANTS"
+      text="Des semelles stimulantes et correctrices évoluant tout au long de la croissance."
+    />
+    <Comp_IconHomeView
+      imageSrc="/public/icons/old-icon.svg"
+      imageAlt="Icone d'une personne agée"
+      title="SÉNIORS"
+      text="Des semelles confortables et respectueuses des déformations pour toutes les pathologies."
+    />
+    <Comp_IconHomeView
+      imageSrc="/public/icons/sport02-icon.svg"
+      imageAlt="Icone d'un sportif"
+      title="SPORTIFS"
+      text="Des semelles plus légères et dynamiques pour de meilleures performances."
+    />
+    <Comp_IconHomeView
+      class="mt-10"
+      imageSrc="/public/icons/shoe-icon.svg"
+      imageAlt="Icone d'une chaussures"
+      title="CHAUSSURES ÉTROITES"
+      text="Des semelles correctrices plus fines, adaptées à un faible volume chaussant."
+    />
+    <Comp_IconHomeView
+      class="mt-10"
+      imageSrc="/public/icons/clock-icon.svg"
+      imageAlt="Icone d'une horloge"
+      title="AFFECTION DE LONGUE DURÉE"
+      text="Des semelles conformes à la législation et au protocole ALD (Affection de Longue Durée)."
+    />
+    <Comp_IconHomeView
+      class="mt-10"
+      imageSrc="/public/icons/foot02-icon.svg"
+      imageAlt="Icone d'un os du pied"
+      title="PIED DIABÉTIQUE"
+      text="Une optimisation des zones de décharges pour une meilleure répartition des appuis."
+    />
   </div>
 
   <h2
