fb3e4f1aed3a194058f71390a3118f998a6eb055
ArnautVasseur <95016869+ArnautVasseur@users.noreply.github.com>
2024-06-12 19:52:09 +0200

    Update 404View.vue

--- src/views/404View.vue
+++ src/views/404View.vue
@@ -12,7 +12,7 @@ window.onscroll=function(){window.scrollTo(x, y);};
   <div :class="{ 'custom-scrollbar-view': specificViewActive }" id="myContainer">
     <header class="flex items-center gap-10">
       <img
-        src="images/IV_logo.png"
+        src=""
         alt="logo_pedicure_podologue"
         class="w-[71.97px] h-[75px] top-5 left-5 ml-5"
       />
